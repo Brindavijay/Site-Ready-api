4faa216765f960d799a73c806c634be70d6ea56d
Brindavijay <brindavijay09@gmail.com>
2016-12-14 22:13:23 -0600

    Update LoginException.java

--- src/main/java/com/icon/item/exception/LoginException.java
+++ src/main/java/com/icon/item/exception/LoginException.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 /**
  * Created by A541351 on 11/20/2016.
  */
-@ResponseStatus(value = HttpStatus.UNAUTHORIZED)
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED
 public class LoginException extends Exception {
 
     public LoginException(String message){
